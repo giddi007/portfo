39c096b317e5be45d78a54a3e28c0ad9e767dea7
adrain <markdonald859@gmail.com>
2025-03-16 18:59:21 +0100

    first commit

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+        /* From Uiverse.io by SmookyDev */ 
+        <section className="flex justify-center items-center h-screen ">
+            <span className="loading loading-infinity loading-xs"></span>
+            <span className="loading loading-infinity loading-sm"></span>
+            <span className="loading loading-infinity loading-md"></span>
+            <span className="loading loading-infinity loading-lg"></span>
+            <span className="loading loading-infinity loading-xl"></span>
+        </section>
+        
+
+    )
+  }
