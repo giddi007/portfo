4d5e6bd74a3a8e11a71c145492d82135a34a869b
adrain <markdonald859@gmail.com>
2025-04-04 17:01:46 +0100

    ab2g2

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ const Home = () => {
   return (
       <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
-          <FloatingNav navItems={navItems} />
           <Hero />
           <Grid />
           <RecentPRojects />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,10 +3,15 @@ import { Spotlight } from './ui/Sportlight'
 import { TextGenerateEffect } from './ui/TextGenerate'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa6'
+import { FloatingNav } from './ui/FloatingNavbar'
+import { navItems } from '@/data'
 
 const Hero = () => {
   return (
-      <div className='pb-20 pt-36'>
+    <>
+        <FloatingNav navItems={navItems} />
+
+       <div className='pb-20 pt-36'>
         <div>
           <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
           <Spotlight className='-top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
@@ -44,7 +49,7 @@ const Hero = () => {
         </div>
         </div>
       </div>
-
+    </>
   )
 }
 
